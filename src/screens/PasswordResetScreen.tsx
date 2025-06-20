fd27d771031ad865e40270dc5276ef65c022f4bf
karthikmohan18 <139226211+karthikmohan18@users.noreply.github.com>
2025-06-20 18:32:11 +0530

    Replace logo with Picture1.png throughout the app

--- src/screens/PasswordResetScreen.tsx
+++ src/screens/PasswordResetScreen.tsx
@@ -325,7 +325,7 @@ export const PasswordResetScreen: React.FC<Props> = ({ onBack }) => {
             </button>
             <div className="flex items-center">
               <img
-                src="/images/zenlit-logo.png"
+                src="/Picture1.png"
                 alt="Zenlit"
                 className="w-8 h-8 object-contain rounded mr-3"
               />
--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -18,7 +18,7 @@ export const WelcomeScreen: React.FC<Props> = ({ onGetStarted }) => {
         {/* Logo */}
         <div className="mb-12">
           <img
-            src="/images/zenlit-logo.png"
+            src="/Picture1.png"
             alt="Zenlit"
             className="w-32 h-32 mx-auto object-contain rounded-lg mb-8"
           />
